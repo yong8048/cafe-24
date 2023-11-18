1ebd2d8292ab10b6dec72af955d9f67bbea70860
bwlee <lbw3973@naver.com>
2023-11-18 23:01:50 +0900

    feat: sidebarStore 추가

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -1,39 +1,15 @@
-import { useState } from "react";
+import { useSidebarStore } from "@/store/sidebarStore";
 
 const HamburgerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  const { setOpen } = useSidebarStore();
 
   return (
-    <div className="relative">
-      <button
-        onClick={toggleMenu}
-        className={`h-4 w-5 flex flex-col justify-between ${
-          isOpen && "text-red-500"
-        }`}
-      >
+    <div className="flex items-center">
+      <button onClick={setOpen} className="h-4 w-5 flex flex-col justify-between">
         <div className="w-full h-0.5 bg-black"></div>
         <div className="w-full h-0.5 bg-black"></div>
         <div className="w-full h-0.5 bg-black"></div>
       </button>
-      <div
-        className={`fixed top-[74px] left-0 h-full bg-gray-100 transition-width duration-300 ease-in-out ${
-          isOpen ? "w-64" : "w-0"
-        } overflow-hidden`}
-      >
-        <a href="#" className="block p-4">
-          메뉴 1
-        </a>
-        <a href="#" className="block p-4">
-          메뉴 2
-        </a>
-        <a href="#" className="block p-4">
-          메뉴 3
-        </a>
-      </div>
     </div>
   );
 };
