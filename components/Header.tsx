b105ebcadba1bb5e2facd22ade9c23d906cad2db
SedanLee <water8048@naver.com>
2023-11-18 16:06:03 +0900

    feat: 헤더 UI추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import HamburgerMenu from "@/components/HamburgerMenu";
+import UserProfile from "./UserProfile";
 
 const Header = () => {
   return (
-    <header className="bg-white h-[74px] flex items-center text-mainColor border-b border-solid border-gray-300">
-      <h1>c 24</h1>
+    <header className="bg-white h-[74px] flex items-center border-b border-solid border-gray-300 p-4 text-black justify-between">
+      <div className="flex gap-4">
+        <HamburgerMenu />
+        <h1>로고</h1>
+        <h1>전체</h1>
+        <h1>무인카페</h1>
+        <h1>일반카페</h1>
+      </div>
+      <div className="flex gap-1 items-center">
+        <UserProfile name="이승용" />
+        <div className="flex gap-1">
+          <h1 className="text-base">이승용</h1>
+          <h1>0</h1>
+        </div>
+      </div>
     </header>
   );
 };
