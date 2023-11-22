6b40c5b8e7f8b4f256f7ad35223856206166ebe2
LeeByoungWook <lbw3973@naver.com>
2023-11-23 00:06:20 +0900

    feat: 선택한 store의 정보 표시

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,19 @@
 import { useSidebarStore } from "@/store/sidebarStore";
 import React from "react";
 import StoreDetial from "./StoreDetial";
+import { useSelectedStore } from "@/store/selectedStore";
+import Intro from "./Intro";
 
 export default function Sidebar() {
   const { isOpen } = useSidebarStore();
-  const temp = "카페만월경 안양점";
-
+  const { data } = useSelectedStore();
   return (
     <aside
       className={`fixed top-[78px] h-full z-50 bg-gray-100 transition-width duration-500 ease-in-out w-[436px] overflow-hidden ${
         isOpen ? "left-0" : "-left-[436px]"
       }`}
     >
-      {temp ? <StoreDetial></StoreDetial> : "포폴이에연"}
+      {data.id ? <StoreDetial /> : <Intro />}
     </aside>
   );
 }
