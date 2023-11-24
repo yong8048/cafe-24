a87542645d971ce945662c30fe1c41232d388fd9
SedanLee <water8048@naver.com>
2023-11-24 14:50:03 +0900

    feat: 햄버거 메뉴 클릭 영역 확대

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -4,8 +4,8 @@ const HamburgerMenu = () => {
   const { setOpen } = useSidebarStore();
 
   return (
-    <div className="flex items-center">
-      <button onClick={setOpen} className="h-4 w-5 flex flex-col justify-between">
+    <div className="flex items-center p-2" onClick={setOpen}>
+      <button className="h-4 w-5 flex flex-col justify-between">
         <div className="w-full h-0.5 bg-black"></div>
         <div className="w-full h-0.5 bg-black"></div>
         <div className="w-full h-0.5 bg-black"></div>
