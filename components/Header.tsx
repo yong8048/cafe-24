f78a03eb14af928c06d2346c5d1f1e90cb68bf35
LeeByoungWook <lbw3973@naver.com>
2023-11-27 01:39:34 +0900

    feat: logo 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
     <header className="bg-white w-full h-[78px] flex items-center border-b border-solid border-gray-300 p-4 text-black justify-between fixed z-50 ">
       <div className="flex gap-10 text-l font-semibold first-of-type:cursor-pointer leading-[66.8px]">
         <HamburgerMenu />
-        <Image src="/Logo.png" alt="Logo" width={150} height={32} className="-ml-6" />
+        <Image src="/Logo.png" alt="Logo" width={150} height={32} className="-ml-6" priority />
         <h1>전체</h1>
         <h1>무인카페</h1>
         <h1>일반카페</h1>
