95bb8aa38a721b87ffa71d9d26c133af2f6b39e2
SedanLee <water8048@naver.com>
2023-11-28 03:28:37 +0900

    fix: 배포에러수정

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -6,12 +6,12 @@ import { collection, getDocs } from "firebase/firestore";
 import { getDownloadURL, getStorage, listAll, ref } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_APIKEY}`,
-  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN}`,
-  projectId: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECTID}`,
-  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET}`,
-  messagingSenderId: `${process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID}`,
-  appId: `${process.env.NEXT_PUBLIC_FIREBASE_APPID}`,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
