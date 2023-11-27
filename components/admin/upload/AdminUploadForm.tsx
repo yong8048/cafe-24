acbf1e611cc5944ecedb5a0affae004bab81fe70
LeeByoungWook <lbw3973@naver.com>
2023-11-28 02:56:34 +0900

    feat: 위도/경도 가져오기 추가

--- components/admin/upload/AdminUploadForm.tsx
+++ components/admin/upload/AdminUploadForm.tsx
@@ -1,11 +1,14 @@
 import { IStoreInfo } from "@/types/firebase";
 import { PostStoreInfo } from "@/utils/firebase";
+import GetGeoLocation from "@/utils/naver";
 import React, { useState } from "react";
 
 const category = {
   name: "지점명",
   type: "카페 타입",
   address: "주소",
+  latitude: "위도",
+  longitude: "경도",
   number: "전화번호",
   table: "테이블",
   parking: "주차",
@@ -28,6 +31,7 @@ const AdminUploadForm = () => {
     toilet: "",
     type: "",
   });
+
   const Post = () => {
     PostStoreInfo(storeData);
   };
@@ -39,19 +43,30 @@ const AdminUploadForm = () => {
     });
   };
 
+  const handleClickSearch = async () => {
+    console.log(storeData.address);
+    const res = await GetGeoLocation(storeData.address);
+    if (res) {
+      setStoreData({ ...storeData, latitude: res.latitude, longitude: res.longitude });
+    }
+  };
+
   return (
     <div className="min-w-[900px] h-full p-10 text-center">
-      <div className="text-xl border rounded-xl py-4 grid justify-center" onChange={handleChange}>
+      <div className="text-xl border rounded-xl py-4 grid justify-center">
         {Object.entries(category).map(([key, value]) => (
           <div key={key} className={`flex gap-10 leading-10 py-2 pl-2 ${key === "address" && "justify-between pr-2"}`}>
             <p className="w-20">{value}</p>
-            {key === "address" ? (
-              <>
-                <p></p>
-                <button className="text-base border border-black px-1 rounded-lg">검색</button>
-              </>
-            ) : (
-              <input className="w-[400px] input-admin" name={key} />
+            <input
+              className={`${key === "address" ? "w-[320px]" : "w-[400px]"} input-admin`}
+              name={key}
+              value={storeData[key as keyof IStoreInfo]}
+              onChange={handleChange}
+            />
+            {key === "address" && (
+              <button className="text-base border border-black px-1 rounded-lg" onClick={handleClickSearch}>
+                검색
+              </button>
             )}
           </div>
         ))}
@@ -62,9 +77,8 @@ const AdminUploadForm = () => {
       </div>
       <div className="mt-5 flex justify-center gap-5 text-2xl text-white">
         <button className="w-[140px] h-[52px] bg-[#3D7FFF] rounded-[20px]" onClick={Post}>
-          승인
+          업로드
         </button>
-        <button className="w-[140px] h-[52px] bg-[#3D7FFF] rounded-[20px]">거부</button>
       </div>
     </div>
   );
