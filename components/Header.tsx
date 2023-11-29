797a098d4667c8db76e3d447d662d8d0fde2a5d8
yong8048 <water8048@naver.com>
2023-11-29 17:00:13 +0900

    feat: 카페 종류 나누기 기능 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,12 @@ import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "@/utils/firebase";
 import Image from "next/image";
 import LoginStatus from "./LoginStatus";
+import { useCafeTypeStore } from "@/store/cafeTypeStore";
 
 const Header = () => {
-  const [isClicked, setIsClicked] = useState(false);
   const [isLogined, setIsLogined] = useState(false);
-
+  const [selected, setSelected] = useState("전체");
+  const { setType } = useCafeTypeStore();
   const provider = new GoogleAuthProvider();
 
   const [userData, setUserData] = useState({
@@ -33,7 +34,7 @@ const Header = () => {
               email: user.email,
             });
           }
-          console.log(user);
+          setIsLogined(true);
         }
       })
       .catch(error => {
@@ -45,13 +46,43 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-white w-full h-[78px] flex items-center border-b border-solid border-gray-300 p-4 text-black justify-between fixed z-50 ">
-      <div className="flex gap-10 text-l font-semibold first-of-type:cursor-pointer leading-[66.8px]">
+    <header className="bg-white w-full h-[78px] flex items-center border-b border-solid border-gray-300 p-4 text-black justify-between ">
+      <div className="flex gap-10 text-l font-semibold leading-[66.8px]">
         <HamburgerMenu />
-        <Image src="/Logo.png" alt="Logo" width={150} height={32} className="-ml-6" priority />
-        <h1>전체</h1>
-        <h1>무인카페</h1>
-        <h1>일반카페</h1>
+        <Image src="/Logo.png" alt="Logo" width={150} height={32} className="-ml-6 cursor-default" />
+        <h1
+          className={`${selected === "전체" && "text-red-400"} cursor-pointer`}
+          onClick={() => {
+            setSelected("전체");
+            setType("전체");
+          }}
+        >
+          전체
+        </h1>
+        <h1
+          className={`${selected === "무인카페" && "text-red-400"} cursor-pointer`}
+          onClick={() => {
+            setSelected("무인카페");
+            setType("무인");
+          }}
+        >
+          무인카페
+        </h1>
+        <h1
+          className={`${selected === "일반카페" && "text-red-400"} cursor-pointer`}
+          onClick={() => {
+            setSelected("일반카페");
+            setType("일반");
+          }}
+        >
+          일반카페
+        </h1>
+        <h1
+          className={`${selected === "즐겨찾기" && "text-red-400"} cursor-pointer`}
+          onClick={() => setSelected("즐겨찾기")}
+        >
+          즐겨찾기
+        </h1>
       </div>
 
       {isLogined ? (
@@ -61,7 +92,6 @@ const Header = () => {
           className="px-4 cursor-pointer"
           onClick={() => {
             handleLogin();
-            setIsLogined(true);
           }}
         >
           로그인
