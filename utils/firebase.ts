6baf3ebb15ab7bbe9c12fd6ca092e56332b207f5
bwlee <lbw3973@naver.com>
2023-11-29 14:43:25 +0900

    test: env 테스트

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -22,8 +22,15 @@ export const auth = () => {
   return getAuth();
 };
 export const GetStoreInfo = async (): Promise<IStoreInfo[]> => {
-  const app = initializeApp(firebaseConfig);
-  console.log(firebaseConfig);
+  const app = initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  });
+
   const db = getFirestore(app);
   const querySnapshot = await getDocs(collection(db, "StoreInfo"));
   const users: IStoreInfo[] = [];
