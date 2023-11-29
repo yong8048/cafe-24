a60d907ce0bf716117214ce125546f72f847b8a3
SedanLee <water8048@naver.com>
2023-11-30 02:21:14 +0900

    feat: 햄버거메뉴 코드 정리

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -1,32 +1,29 @@
+"use client";
 import { useSidebarStore } from "@/store/sidebarStore";
-import { useState } from "react";
 
+const activeStyle = [
+  { condition: "transform translate-y-1 rotate-45" },
+  { condition: "opacity-0 -translate-x-20" },
+  { condition: "transform -translate-y-2.5 -rotate-45" },
+];
 const HamburgerMenu = () => {
-  const { setToggle } = useSidebarStore();
-  const [isActive, setIsActive] = useState(false);
+  const { isOpen, setToggle } = useSidebarStore();
+
   const handleActive = () => {
     setToggle();
-    setIsActive(!isActive);
   };
 
   return (
     <div className="flex items-center p-2" onClick={handleActive}>
       <button className="h-4 w-5 flex flex-col justify-between">
-        <div
-          className={`w-full h-0.5 bg-black transition-all duration-200 ease-out shadow-lg ${
-            isActive ? "" : "transform translate-y-1 rotate-45"
-          } `}
-        ></div>
-        <div
-          className={`w-full h-0.5 bg-black transition-all duration-200 ease-out shadow-lg ${
-            isActive ? "" : "opacity-0 -translate-x-20"
-          }`}
-        ></div>
-        <div
-          className={`w-full h-0.5 bg-black transition-all duration-200 ease-out shadow-lg ${
-            isActive ? "" : "transform -translate-y-2.5 -rotate-45"
-          }`}
-        ></div>
+        {activeStyle.map((style, index) => (
+          <div
+            key={index}
+            className={`w-full h-0.5 bg-black transition-all duration-200 ease-out shadow-lg ${
+              isOpen && style.condition
+            }`}
+          ></div>
+        ))}
       </button>
     </div>
   );
