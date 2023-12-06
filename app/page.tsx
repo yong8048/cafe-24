ae9d39a75b01cff007ef735b756f2752b44e0cb3
LeeByoungWook <lbw3973@naver.com>
2023-12-06 23:37:31 +0900

    feat: report 버튼 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
+import { useState } from "react";
 import Map from "@/components/Map";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import ReportButton from "@/components/ReportButton";
 
 export default function Home() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div>
+    <>
       <Header />
       <Sidebar />
       <Map />
-    </div>
+      <ReportButton />
+    </>
   );
 }
---
+++ components/ReportButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReportButton = () => {
+  return (
+    <button className="w-[140px] h-[54px] text-2xl rounded-2xl absolute bottom-5 right-5 bg-[#3d7fff] text-white">
+      제보하기
+    </button>
+  );
+};
+
+export default ReportButton;
