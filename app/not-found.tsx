20d3de03d29ee8cdb11cf0e20f6ab93d4b66e9ac
LeeByoungWook <lbw3973@naver.com>
2023-12-11 13:58:52 +0900

    design: not-found 페이지 구현

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,21 +4,15 @@ import Link from "next/link";
 function NotFound() {
   return (
     <section className="text-center">
-      <Image src="/Logo.png" width={200} height={100} alt="logo" />
-      <div className="my-16 flex flex-col gap-4 break-keep leading-6">
-        <h3 className="mb-10 text-2xl font-bold">페이지가 없습니다</h3>
+      <div className="my-16 flex flex-col gap-4 break-keep leading-6 items-center">
+        <Image src="/Logo.png" width={200} height={100} alt="logo" />
+        <h3 className="my-5 text-2xl font-bold">페이지가 없습니다</h3>
         <p>주소가 잘못되었거나 바뀐 것 같습니다.</p>
         <p>다시 확인해주시겠어요?</p>
       </div>
-      <nav className="mx-auto flex max-w-[500px] gap-2">
+      <nav className="mx-auto flex max-w-[500px] gap-2 items-center justify-center">
         <Link
-          className="flex h-[56px] w-full items-center justify-center rounded-md border-1 border-primary-normal bg-white font-bold text-primary-normal"
-          href="/"
-        >
-          뒤로 가기
-        </Link>
-        <Link
-          className="flex h-[56px] w-full items-center justify-center rounded-md bg-primary-normal font-bold text-white"
+          className="h-[56px] leading-[56px] text-center rounded-md font-bold text-white bg-[#3d7fff] px-4"
           href="/"
         >
           홈 화면으로 이동
