3caf9a8883c6e4f3e2800caf13648424d7688c5f
LeeByoungWook <lbw3973@naver.com>
2023-12-13 02:26:33 +0900

    test: ios 환경 테스트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,8 +41,8 @@ const config: Config = {
           "100%": { transform: "scale(1.2)" },
         },
         fontweight: {
-          "0%, 100%": { fontWeight: "normal", color: "gray" },
-          "50%": { fontWeight: "bold", color: "black" },
+          "0%, 100%": { fontweight: "normal", color: "gray" },
+          "50%": { fontweight: "bold", color: "black" },
         },
       },
     },
