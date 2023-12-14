1dc8f587afa11b0f0008527180e474a732af49a0
LeeByoungWook <lbw3973@naver.com>
2023-12-14 14:51:53 +0900

    test: ios chrome 테스트(sidebar fixed -> absolute)

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
   const { isReportClicked } = useReportClickStore();
   return (
     <aside
-      className={`test_test fixed sm:top-[78px] top-[44px] z-40 sm:h-main_section_sm h-main_section bg-white transition-width duration-500 ease-in-out sm:w-[436px] w-full border-r border-gray-300 ${
+      className={`test_test absolute sm:top-[78px] top-[44px] z-40 sm:h-main_section_sm h-main_section bg-white transition-width duration-500 ease-in-out sm:w-[436px] w-full border-r border-gray-300 ${
         isOpen ? "left-0" : "sm:-left-[436px] -left-[640px]"
       }`}
     >
