e385912cdd63c79c5fc89dfa3eaac2708fe5cd5c
LeeByoungWook <lbw3973@naver.com>
2023-12-15 15:23:50 +0900

    feat: metadata 작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,46 @@
-"use client";
 import localFont from "next/font/local";
 import "./globals.css";
 import ReactQueryProvider from "@/components/ReactQueryProvider";
-import { useEffect } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://cafe-24.vercel.app"),
+  title: "새벽 감성 - 24시 카페",
+  description: "24시 운영하는 카페들을 모아 찾아보는 사이트",
+  keywords: ["24시", "24시카페", "무인카페", "새벽감성", "밤카페", "카공"],
+  openGraph: {
+    title: "새벽 감성 - 24시 카페",
+    description: "24시 운영하는 카페들을 모아 찾아보는 사이트",
+    url: "https://cafe-24.vercel.app",
+    siteName: "새벽 감성 - 24시 카페",
+    images: [
+      {
+        url: "/Logo.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://cafe-24.vercel.app",
+    title: "새벽 감성 - 24시 카페",
+    description: "24시 운영하는 카페들을 모아 찾아보는 사이트",
+    images: ["/Logo.png"],
+  },
+};
+
 const pretendard = localFont({
   src: "../styles/PretendardVariable.woff2",
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    function setVhProperty() {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    }
-
-    setVhProperty();
-    window.addEventListener("resize", setVhProperty);
-
-    return () => {
-      window.removeEventListener("resize", setVhProperty);
-    };
-  }, []);
-
   return (
     <html lang="ko">
       <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
-        <title>새벽 감성 - 24시 카페</title>
         <meta name="viewport" content="width=device-width, minimal-ui, viewport-fit=cover" />
         <meta name="google-site-verification" content="q9x7v3OCZ9KTW_JeVWCs9PFKl3oQCviK82JokLkQmzY" />
       </head>
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,23 @@ import Map from "@/components/Map";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import ReportButton from "@/components/ReportButton";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    function setVhProperty() {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+
+    setVhProperty();
+    window.addEventListener("resize", setVhProperty);
+
+    return () => {
+      window.removeEventListener("resize", setVhProperty);
+    };
+  }, []);
+
   return (
     <>
       <Header />
