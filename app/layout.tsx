6e35b7acf063bbb53ef0b38ea6cf1d153e688350
SedanLee <water8048@naver.com>
2024-01-23 16:46:01 +0900

    feat: 구글 애드센스 메타태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content="width=device-width, minimal-ui, viewport-fit=cover" />
         <meta name="google-site-verification" content="q9x7v3OCZ9KTW_JeVWCs9PFKl3oQCviK82JokLkQmzY" />
         <meta name="naver-site-verification" content="61c718a8f06eeaba81289acd4e5e5cc5ac894010" />
+        <meta name="google-adsense-account" content="ca-pub-7406057926683598"></meta>
       </head>
       <body>
         <ReactQueryProvider>
